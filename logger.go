6623a4b4431acc4c117f50e6e329e4914651c282
Teea Alarto <theteea@gmail.com>
2025-05-24 16:51:46 +0100

    Add LogAttrs()

--- logger.go
+++ logger.go
@@ -28,3 +28,7 @@ func (l *Logger) Error(ctx context.Context, msg string, args ...any) {
 func (l *Logger) GetHandler() slog.Handler {
 	return l.logger.Handler()
 }
+
+func (l *Logger) LogAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	l.logger.LogAttrs(ctx, level, msg, attrs...)
+}
